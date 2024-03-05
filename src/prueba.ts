ee91c372bf0b766939a370a4c033c8711dcc8e39
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2024-03-05 11:37:10 -0600

    let var + ` " ' + innerHtml + getelementbyid

---
+++ src/prueba.ts
@@ -0,0 +1,19 @@
+// document.querySelector<HTMLButtonElement>('#accionate')!.addEventListener('click', () => {
+//     alert('Accionate')
+//     // console.log('Accionate')
+// })
+
+document.getElementById('accionate')!.addEventListener('click', () => {
+    let email = document.getElementById('floatingInput') as HTMLInputElement;
+    let pass = document.getElementById('floatingPassword') as HTMLInputElement;
+    let resultado = document.getElementById('resultado') as HTMLElement;
+
+    console.log(email.value);
+    console.log(pass.value);
+
+    resultado.innerHTML += ` Email: ${email.value} <br> Password: ${pass.value}`;
+
+
+
+
+});
